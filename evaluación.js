a4528671a7834fb8908767b9c06ff4fb8e36c8ed
DarckSould <darteaga702@gmail.com>
2025-06-23 03:14:01 -0500

    Alternativas para móviles v4

--- evaluación.js
+++ evaluación.js
@@ -14,7 +14,7 @@ async function enviarEvaluacion(event) {
   if (respuestas.length < 5) return alert('Completa todas las preguntas');
 
   try {
-    await new Promise((r) => setTimeout(r, 1000));
+    await new Promise((r) => setTimeout(r, 2000));
     const res = await fetch(
       'https://backend-salud-mental.onrender.com/evaluaciones',
       {
